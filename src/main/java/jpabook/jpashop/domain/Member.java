e11d316068a39e173022e26c920358e026f5227c
rnrl1215 <d1215@naver.com>
2021-08-22 20:38:48 +0900

    modified strategy of entity

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -3,6 +3,8 @@ package jpabook.jpashop.domain;
 import net.bytebuddy.asm.Advice;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.time.Period;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,12 +15,24 @@ public class Member extends BaseEntity {
     @Column(name = "MEMBER_ID")
     private Long id;
 
-    // 이렇게 규약을 정해주면 알아보기 쉽다.
     @Column(length = 10)
     private String name;
+
+    // period
+    /*
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    */
+    private Period workPeriod;
+
+    // address
+    // 이렇게 규약을 정해주면 알아보기 쉽다.
+    /*
     private String city;
     private String street;
     private String zipCode;
+    */
+    private Address address;
 
 
     @OneToMany(mappedBy = "member")
---
+++ src/main/java/jpabook/jpashop/domain/Period.java
@@ -0,0 +1,24 @@
+import javax.persistence.Embedded;
+import java.time.LocalDateTime;pac
+
+@Embedded
+public class Period {
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDateTime endDate) {
+        this.endDate = endDate;
+    }
+}

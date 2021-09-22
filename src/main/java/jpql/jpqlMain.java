fa5e9dba771b62229d0240c8f9d82b1b58ee13ad
skahn1215 <d1215@naver.com>
2021-09-22 18:10:16 +0900

    added JPQL package

---
+++ src/main/java/jpql/jpqlMain.java
@@ -0,0 +1,34 @@
+package jpql;
+
+
+
+import jpabook.jpashop.domain.Member;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+
+public class jpqlMain {
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            em.createQuery("select m From Member m where m.username like '%kim%'");
+
+            Member m = new Member();
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}

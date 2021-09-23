46fc8083fbf9548652020f78bc204b4717b6f0a9
skahn1215 <d1215@naver.com>
2021-09-23 14:02:28 +0900

    added JPQL package

--- src/main/java/jpql/jpqlMain.java
+++ src/main/java/jpql/jpqlMain.java
@@ -24,7 +24,7 @@ public class jpqlMain {
 
         try {
             // JPQL 예제
-/*
+            /*
             String qlString = "select m from Member m where m.username like '%kim%'";
             List<Member> result = em.createQuery(qlString,
                             Member.class).getResultList();
@@ -32,17 +32,46 @@ public class jpqlMain {
             for (Member member : result) {
                 System.out.println("member =" + member);
             }
-*/
+            */
 
             // Criteria 예제
+            // 실무에서는 사용하지 않는다.
             // 단순한 예제는 쉽지만 복잡한 쿼리에서는 힘들다.
             // 장점 동적쿼리 및 문법오류를 줄일수 있다.
+
+            /*
             CriteriaBuilder cb = em.getCriteriaBuilder();
             CriteriaQuery<Member> query = cb.createQuery(Member.class); // 멤버에 대한쿼리를 한다.
             Root<Member> m = query.from(Member.class);
 
-            CriteriaQuery<Member> cq =  query.select(m).where(cb.equal(m.get("username"),"kim")); // equal 로 비교
+            CriteriaQuery<Member> cq =  query.select(m);
+
+            // 아래 처럼 동적 쿼리를 짤수 있다.
+            // 사용자 이름 여부에대해서 동적쿼리를 다음과같이 작성함.
+            String username = "kim";
+            if (username != null) {
+                cq.where().where(cb.equal(m.get("username"),"kim")); // equal 로 비교
+            }
+
             List<Member> resultList = em.createQuery(cq).getResultList();
+            */
+
+
+            // Native SQL
+            Member member = new Member();
+            member.setUserName("TSET_NAME");
+            em.persist(member);
+            em.flush();
+            List <Object[]> resultList = em.createNativeQuery("select MEMBER_ID, city, street, zipcode, USER_NAME from MEMBER")
+                    .getResultList();
+
+            for(Object[] row : resultList) {
+                System.out.println(row[0]);
+                System.out.println(row[1]);
+                System.out.println(row[2]);
+                System.out.println(row[3]);
+                System.out.println(row[4]);
+            }
 
             tx.commit();
         } catch (Exception e) {
